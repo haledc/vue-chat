39b7146411514f21f91b2bc5f6cad2da83876428
Hale Deng <haledeng88@gmail.com>
2018-06-20 23:35:05 +0800

    update router

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,26 +1,24 @@
 export default [
-  {
-    path: '/',
-    redirect: '/login'
-  },
+  {path: '/', redirect: '/login'},
+
+  {path: '/login', component: () => import('pages/login')},
+
+  {path: '/register', component: () => import('pages/register')},
+
+  {path: '/update-info/:type', component: () => import('pages/update-info')},
 
   {
-    path: '/login',
-    component: () => import('pages/login')
+    path: '/dashboard',
+    component: () => import('layouts/default'),
+    children: [
+      {path: 'genius', component: () => import('pages/genius')},
+      {path: 'boss', component: () => import('pages/boss')},
+      {path: 'msg', component: () => import('pages/msg')},
+      {path: 'user-center', component: () => import('pages/user-center')}
+    ]
   },
 
-  {
-    path: '/register',
-    component: () => import('pages/register')
-  },
+  {path: '/chat/:target', component: () => import('pages/chat')},
 
-  {
-    path: '/update-info/:type',
-    component: () => import('pages/update-info')
-  },
-
-  {
-    path: '*',
-    component: () => import('pages/404')
-  }
+  {path: '*', component: () => import('pages/404')}
 ]
