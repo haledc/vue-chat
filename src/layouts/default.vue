98424ca78050ebd2ca7c7388270d909b50a52870
Hale Deng <haledeng88@gmail.com>
2018-06-20 23:34:20 +0800

    add dashboard layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,81 +1,110 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
+  <q-layout>
     <q-layout-header>
-      <q-toolbar
-        color="primary"
-        :glossy="$q.theme === 'mat'"
-        :inverted="$q.theme === 'ios'"
-      >
-        <q-btn
-          flat
-          dense
-          round
-          @click="leftDrawerOpen = !leftDrawerOpen"
-          aria-label="Menu"
-        >
-          <q-icon name="menu" />
-        </q-btn>
-
-        <q-toolbar-title>
-          Quasar App
-          <div slot="subtitle">Running on Quasar v{{ $q.version }}</div>
-        </q-toolbar-title>
+      <q-toolbar>
+        <q-toolbar-title class="text-center">{{toolTitle}}</q-toolbar-title>
       </q-toolbar>
     </q-layout-header>
-
-    <q-layout-drawer
-      v-model="leftDrawerOpen"
-      :content-class="$q.theme === 'mat' ? 'bg-grey-2' : null"
-    >
-      <q-list
-        no-border
-        link
-        inset-delimiter
-      >
-        <q-list-header>Essential Links</q-list-header>
-        <q-item @click.native="openURL('http://quasar-framework.org')">
-          <q-item-side icon="school" />
-          <q-item-main label="Docs" sublabel="quasar-framework.org" />
-        </q-item>
-        <q-item @click.native="openURL('https://github.com/quasarframework/')">
-          <q-item-side icon="code" />
-          <q-item-main label="GitHub" sublabel="github.com/quasarframework" />
-        </q-item>
-        <q-item @click.native="openURL('https://discord.gg/5TDhbDg')">
-          <q-item-side icon="chat" />
-          <q-item-main label="Discord Chat Channel" sublabel="https://discord.gg/5TDhbDg" />
-        </q-item>
-        <q-item @click.native="openURL('http://forum.quasar-framework.org')">
-          <q-item-side icon="record_voice_over" />
-          <q-item-main label="Forum" sublabel="forum.quasar-framework.org" />
-        </q-item>
-        <q-item @click.native="openURL('https://twitter.com/quasarframework')">
-          <q-item-side icon="rss feed" />
-          <q-item-main label="Twitter" sublabel="@quasarframework" />
-        </q-item>
-      </q-list>
-    </q-layout-drawer>
-
     <q-page-container>
-      <router-view />
+      <router-view/>
     </q-page-container>
+    <q-layout-footer class="col-xs-12">
+      <q-tabs glossy align="justify" class="shadow-2" v-model="activeTab">
+        <q-route-tab exact v-if="user.type === 'boss' " slot="title" icon="accessibility" label="牛人" to="/dashboard/genius"/>
+        <q-route-tab exact v-else slot="title" icon="accessibility" label="BOSS" to="/dashboard/boss"/>
+        <q-route-tab exact slot="title" :alert="alert" icon="message" label="消息" to="/dashboard/msg"/>
+        <q-route-tab exact slot="title" icon="perm_identity" label="个人" to="/dashboard/user-center"/>
+      </q-tabs>
+    </q-layout-footer>
   </q-layout>
 </template>
 
 <script>
-import { openURL } from 'quasar'
+  import {mapMutations, mapGetters} from 'vuex'
 
-export default {
-  name: 'LayoutDefault',
-  data () {
-    return {
-      leftDrawerOpen: this.$q.platform.is.desktop
+  export default {
+    data() {
+      return {
+        activeTab: '',
+        alert: false,
+        receiveData: '',
+        routerList: [
+          {
+            path: '/genius',
+            title: '牛人列表'
+          },
+          {
+            path: '/boss',
+            title: 'BOSS列表'
+          },
+          {
+            path: '/msg',
+            title: '消息列表'
+          },
+          {
+            path: '/user-center',
+            title: '个人中心'
+          }
+        ]
+      }
+    },
+    created() {
+      // 根据路径获取活动按钮的值
+      this.activeTab = this.$route.path.split('/')[2]
+      this.getTargetList()
+      this.getChatMsg()
+      this.receiveMsg()
+    },
+    computed: {
+      toolTitle() {
+        const path = this.$route.path
+        const target = this.routerList.find(item => ('/dashboard' + item.path === path))
+        return target.title
+      },
+      ...mapGetters('user', ['user']),
+      ...mapGetters('chat', ['chatMsg'])
+    },
+    methods: {
+      getTargetList() {
+        const type = this.user.type === 'boss' ? 'genius' : 'boss'
+        this.$axios
+          .get(`/user/list?type=${type}`)
+          .then(res => {
+            if (res.data.status === 0) {
+              this.setTargetList(res.data.result)
+            }
+          })
+      },
+      getChatMsg() {
+        this.$axios
+          .get('/user/chatMsg')
+          .then(res => {
+            if (res.data.status === 0) {
+              this.setChatMsg(res.data.result.chat)
+            }
+          })
+      },
+      receiveMsg() {
+        this.$socket.removeAllListeners()
+        this.$socket.on('receiveMsg', data => {
+          console.log('receiveMsg at dashboard')
+          this.receiveData = data
+        })
+      },
+      ...mapMutations('user', ['setTargetList']),
+      ...mapMutations('chat', ['setChatMsg'])
+    },
+    watch: {
+      // 监听路由变化，调整activeBtn的值(解决导航守卫的跳转时出现的bug)
+      '$route'() {
+        this.activeTab = this.$route.path.split('/')[2]
+      },
+      receiveData() {
+        this.chatMsg.push(this.receiveData)
+        this.setChatMsg(this.chatMsg)
+      }
     }
-  },
-  methods: {
-    openURL
   }
-}
 </script>
 
 <style>
