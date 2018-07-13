1f4a0b629707b8653c1850867032f08dba2a1d9a
Hale Deng <haledeng88@gmail.com>
2018-07-13 23:42:12 +0800

    update

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,7 @@
-const router = require('koa-router')()
-
+const Router = require('koa-router')
 const user = require('./user')
+
+const router = new Router()
 
 router.use(user.routes()).use(user.allowedMethods())
 
