81fc9797e3a91d517284f1b8496a84ba0aacc82c
Hale Deng <haledeng88@gmail.com>
2018-07-26 00:32:20 +0800

    packaging axios request

---
+++ src/assets/api/index.js
@@ -0,0 +1,53 @@
+import axios from 'axios'
+
+import {createError} from '../utils'
+
+const request = axios.create({
+  baseURL: '/'
+})
+
+const handleRequest = (request) => {
+  return new Promise((resolve, reject) => {
+    request
+      .then(res => {
+        const data = res.data
+        if (!data) {
+          return reject(createError(400, 'no result'))
+        }
+        if (!data.success) {
+          reject(createError(400, data.message))
+        }
+        resolve(data.result)
+      })
+      .catch(err => {
+        const res = err.response
+        if (res.status === 401) {
+          reject(createError(401, 'need auth'))
+        }
+      })
+  })
+}
+
+export default {
+  login(username, password) {
+    return handleRequest(request.post('/user/login', {username, password}))
+  },
+  register(username, password, type) {
+    return handleRequest(request.post('/user/register', {username, password, type}))
+  },
+  updateInfo(updateData) {
+    return handleRequest(request.post('/user/updateInfo', updateData))
+  },
+  getTargetList(type) {
+    return handleRequest(request.get('/user/list', {params: {type}})) // 注意：params是一个对象
+  },
+  logout() {
+    return handleRequest(request.post('/user/logout'))
+  },
+  getChatMsg() {
+    return handleRequest(request.get('/user/chatMsg'))
+  },
+  readMsg(from) {
+    return handleRequest(request.post('/user/readMsg', {from}))
+  }
+}
