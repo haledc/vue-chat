fd6404c5679aeba75955b38cd044e5da2f5455a3
Hale Deng <haledeng88@gmail.com>
2019-01-05 16:31:25 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -42,9 +42,6 @@ module.exports = {
     'import/no-extraneous-dependencies': 0,
 
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-
-    'indent': 0,
-    'space-before-function-paren': 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
 }
